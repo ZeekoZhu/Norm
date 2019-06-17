aead3beb0c68a4c5c17d7c79d0d6f8745fb1224a
ZeekoZhu <vaezt@outlook.com>
2019-06-17 23:12:37 +0800

    fix(SqlServerCompiler): fix row number cte generation

--- Norm/Compiler/SqlServerCompiler.fs
+++ Norm/Compiler/SqlServerCompiler.fs
@@ -1,4 +1,6 @@
 module Norm.Compiler.SqlServerCompiler
+open System.Collections.Generic
+open System.Text
 open Norm.BuildingBlock
 open Norm.Compiler.DefaultCompiler
 open Norm.Compiler.DefaultCompiler.Buffer
@@ -15,6 +17,7 @@ type SqlServerCompilerOptions =
     
 type SqlServerContext =
     { mutable RowNumberCtx: int
+      mutable GeneratedCte: Stack<CteExpression>
     }
 
 type RowNumberExpression(orderBy: OrderClause, index:int, perPage: int) =
@@ -40,10 +43,9 @@ let onCompilePagination ctx (pagination: PaginationClause) (opt: SqlServerCompil
         write " ROWS ONLY" ctx
         true
 
-let getNextRowNumberColumn (ctx: CompilerContext) =
+let useRowNumber (ctx: CompilerContext) =
     let ctx = ctx.ExtraContext.Value :?> SqlServerContext
     ctx.RowNumberCtx <- ctx.RowNumberCtx + 1
-    sprintf "__RowNumber_%i" ctx.RowNumberCtx
 
 let getCurrentRowNumberColumn (ctx: CompilerContext) =
     let ctx = ctx.ExtraContext.Value :?> SqlServerContext
@@ -57,13 +59,43 @@ let getCurrentRowNumberTo (ctx: CompilerContext) =
     let ctx = ctx.ExtraContext.Value :?> SqlServerContext
     sprintf "__RowNumber_%i_to" ctx.RowNumberCtx
 
+let getCurrentRowNumberWrapper (ctx: CompilerContext) =
+    let ctx = ctx.ExtraContext.Value :?> SqlServerContext
+    sprintf "__RowNumber_Wrapper_%i" ctx.RowNumberCtx
+
+/// Convert
+///     SELECT Foo.Bar, Foo.Baa
+/// to
+///     SELECT Foo.Bar, Foo.Baa, __RowNumber_1
 let convertToSelectRowNumber (select: SelectClause) orderBy (pagination: PaginationClause) (ctx: CompilerContext) =
     let rowNumber = RowNumberExpression(orderBy, pagination.Index, pagination.PerPage)
-    let rowNumber = alias rowNumber (getNextRowNumberColumn ctx)
+    let rowNumber = alias rowNumber (getCurrentRowNumberColumn ctx)
     let cols = Array.append select.Columns [|rowNumber |> ColumnsOperand.Identifier |]
     let newSelect = SelectClause(cols)
     newSelect
 
+/// Convert
+/// SELECT Foo.Bar, Foo.Far AS Ffar
+/// to
+/// SELECT Bar, Ffar
+let simplifySelectClause (select: SelectClause) =
+    let removeColumnAccessor (operand: ColumnsOperand) =
+        match operand with
+        | Alias x -> x.Alias
+        | ComputeAlias x -> x.Alias
+        | ColumnsOperand.Identifier x ->
+            match x with
+            | :? MemberAccessExpression as ma ->
+                ma.Child
+            | :? AliasExpression as x -> x.Alias
+            | _ -> x
+        |> ColumnsOperand.Identifier
+    select.Columns
+    |> Array.map removeColumnAccessor
+    |> SelectClause
+
+/// Convert pagination to
+/// WHERE __RowNumber_1 between @__RowNumber_1_from and @__RowNumber_1_to
 let convertToWhereRowNumber (pagination: PaginationClause) (ctx: CompilerContext) =
     let rowNumberCol = getCurrentRowNumberColumn ctx |> TableOrColumnExpression
     let rowNumberCol = rowNumberCol :> IdentifierExpression |> Identifier
@@ -74,27 +106,82 @@ let convertToWhereRowNumber (pagination: PaginationClause) (ctx: CompilerContext
     WhereClause(betweenCondition :> OperatorExpression |> Predicate)
     |> Some
 
+/// Convert
+/// SELECT Foo.Bar FROM Foo
+/// to
+/// WITH [__RowNumber_Wrapper_1] AS (
+///     SELECT Foo.Bar, __RowNumber_1 FROM Foo
+/// )
+/// SELECT Bar FROM [__RowNumber_Wrapper_1]
+/// WHERE [__RowNumber_1] BETWEEN [..] AND [..]
+let convertToRowNumberCTESelectStmt (stmt: SelectStatement) (ctx: CompilerContext) =
+    useRowNumber ctx
+    let selectInCte = convertToSelectRowNumber stmt.Select stmt.Order.Value stmt.Pagiantion.Value ctx
+    let resultSelect = simplifySelectClause stmt.Select
+    stmt.Select <- selectInCte
+    let wrapperTable = table (getCurrentRowNumberWrapper ctx)
+    let wrapperWhere = convertToWhereRowNumber stmt.Pagiantion.Value ctx
+    stmt.Pagiantion <- None
+    stmt.Order <- None
+    let ctes = CSList()
+    if stmt.Ctes.IsSome then
+        ctes.AddRange stmt.Ctes.Value.Tables
+        stmt.Ctes <- None
+    let cte = CteExpression(wrapperTable, stmt)
+    let wrapperStmt = SelectStatement(FromClause(DataSet.Table wrapperTable), resultSelect)
+    let ctx = ctx.ExtraContext.Value :?> SqlServerContext
+    ctx.GeneratedCte.Push(cte)
+    wrapperStmt.Where <- wrapperWhere
+    wrapperStmt
+
+let onCompileWithClause
+    (ctx: CompilerContext) (withClause: WithClause) (compilerOpt: SqlServerCompilerOptions) =
+    if compilerOpt.UseRowNumberForPaging then
+        let tempCtx =
+            { ctx with Buffer = StringBuilder() }
+        let sqlCtx = ctx.ExtraContext.Value :?> SqlServerContext
+        DefaultCompiler.compileWithClause tempCtx withClause
+        while sqlCtx.GeneratedCte.Count > 0 do
+            write ", " tempCtx
+            DefaultCompiler.compileCte tempCtx (sqlCtx.GeneratedCte.Pop())
+        ctx.Buffer.Insert(0, tempCtx.Buffer.ToString()).Append(" ") |> ignore
+        true
+    else false
+
 let onCompileSelectStatement
     (ctx: CompilerContext) (stmt: SelectStatement) (compilerOpt: SqlServerCompilerOptions) =
     match ctx.ExtraContext with
-    | None -> ctx.ExtraContext <- Some ({ RowNumberCtx = 0 } :> obj)
+    | None ->
+        ctx.ExtraContext <-
+            Some ({ RowNumberCtx = 0;
+                    GeneratedCte = Stack() } :> obj)
     | Some _ -> ()
-    if compilerOpt.UseRowNumberForPaging && stmt.Pagiantion.IsSome then
-        match stmt.Order with
-        | None -> failwith "Pagination must have order by clause"
-        | Some orderBy ->
-            let newSelect =
-                convertToSelectRowNumber stmt.Select orderBy stmt.Pagiantion.Value ctx
-            stmt.Select <- newSelect
-            let selectAll =
-                SelectClause([|ColumnsOperand.Identifier (AllColumnsExpression())|])
-            let newStmt =
-                SelectStatement(FromClause(DataSet.SubQuery (AliasExpression<_>(stmt, TableOrColumnExpression("__RowNumber_Wrapper")))), selectAll)
-            let newWhere =
-                convertToWhereRowNumber stmt.Pagiantion.Value ctx
-            newStmt.Where <- newWhere
-            stmt.Pagiantion <- None
-            compile ctx newStmt
+    if compilerOpt.UseRowNumberForPaging then
+        let stmt =
+            if stmt.Pagiantion.IsSome then
+                match stmt.Order with
+                | None -> failwith "Query with pagination must have ORDER BY clause"
+                | Some _ ->
+                    let newStmt = convertToRowNumberCTESelectStmt stmt ctx
+                    newStmt
+            else stmt
+        compile ctx stmt.Select
+        write " " ctx
+        compile ctx stmt.From
+        write " " ctx
+
+        compileOptionClause ctx stmt.Where
+        write " " ctx
+        compileOptionClause ctx stmt.Group
+        write " " ctx
+        compileOptionClause ctx stmt.Having
+        write " " ctx
+        compileOptionClause ctx stmt.Order
+        write " " ctx
+        match stmt.Ctes with
+        | Some x when x.Tables.Count > 0 ->
+            compile ctx x
+        | _ -> ()
         true
     else false
 
@@ -108,6 +195,8 @@ let onCompileExpression: SqlServerCompilerOptions -> OnCompileExpression =
         | :? RowNumberExpression as x ->
             compileRowNumber ctx x
             true
+        | :? WithClause as x ->
+            onCompileWithClause ctx x opt
         | _ -> false
 
 let createDefaultContext (opt) =
